072c30a4f8ed644d03c6e7866fabe631b66d97b2
UndersenF <begintolife@mail.ru>
2024-05-14 23:59:53 +0300

    Mistakes was redacted.

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,19 @@ public class Main {
 
         int age = 16;
         if (age >= 18) {
-            System.out.println("Возраст 18 или больше");
+            System.out.printf("Если возраст человека равен %s то он совершеннолетний", age);
         } else {
-            System.out.println("Возраст совершеннолетия еще не наступил, нужно немного подождать.");
+            System.out.printf("Если возраст человека равен %s то он несовершеннолетний, нужно немного подождать\n", age);
         }
 
         //Task 2
         System.out.println();
 
-        int temperature = 6;
+        int temperature = 5;
         if (temperature >= 5) {
-            System.out.println("Сегодня тепло, можно идти без шапки");
+            System.out.printf("На улице %s градусов, можно идти без шапки\n", temperature);
         } else if (temperature < 5) {
-            System.out.println("На улице холодно, нужно надеть шапку");
+            System.out.printf("На улице %s градусов, нужно надеть шапку\n", temperature);
         }
 
 //        Task3
@@ -26,10 +26,10 @@ public class Main {
         int carSpeed = 30;
 
         if (carSpeed > speedLimit) {
-            System.out.println("Скорость превышена. Притормози!");
+            System.out.printf("Скорость равна  %s км/ч это гразит штрафом. Притормози!\n", carSpeed);
         }
-        else if (carSpeed < speedLimit) {
-            System.out.println("Скорость нормальная. Педаль в пол!");
+        else if (carSpeed <= speedLimit) {
+            System.out.printf("Скорость равна  %s км/ч все нормально. Педаль в пол!\n", carSpeed);
         }
 
 //        Task4
@@ -37,13 +37,13 @@ public class Main {
 
         int humanAge = 28;
 
-        if (humanAge > 2 && humanAge < 6) {
+        if (humanAge > 2 && humanAge <= 6) {
             System.out.printf("Если возраст человека равен %S то ему пора в Детский Сад\n", humanAge);
         }
-        else if (humanAge > 7 && humanAge < 17) {
+        else if (humanAge >= 7 && humanAge <= 17) {
             System.out.printf("Если возраст человека равен %S то ему пора в Школу\n", humanAge);
         }
-        else if (humanAge > 18 && humanAge < 24) {
+        else if (humanAge >= 18 && humanAge <= 24) {
             System.out.printf("Если возраст человека равен %S то ему пора в Универсиете\n", humanAge);
         }
         else if (humanAge > 24) {
@@ -73,24 +73,24 @@ public class Main {
         int standingPlaces = sizeOfWagon - sitingPlaces;
 
         int countOfSittingPassengers = 60;
-        int countOfStandingPassengers = 41;
+        int countOfStandingPassengers = 42;
         int emptyPlaces = sizeOfWagon - countOfSittingPassengers - countOfStandingPassengers;
         sitingPlaces -= countOfSittingPassengers;
         standingPlaces -= countOfStandingPassengers;
 
-        if (emptyPlaces == 0) {
+        if (emptyPlaces <= 0) {
             System.out.println("Вагон забит свободных мест нет");
         }
-        else if (emptyPlaces != 0 ) {
+        else if (emptyPlaces > 0 ) {  //может быть истинным даже если нет свободных мест???
             System.out.printf("В вагоне остались свободные места. %s сидячих и %s стоячих\n", sitingPlaces, standingPlaces);
         }
 
 //        task7
         System.out.println();
 
-        int one = 0;
-        int two = 1;
-        int three = 19;
+        int one = 10;
+        int two = 10;
+        int three = 0;
 
         if (one > two && one >three) {
             System.out.println(one + " самое большое число!");
@@ -101,6 +101,19 @@ public class Main {
         else if (three > one && three > two) {
             System.out.println(three + " самое большое число!");
         }
+        else if (one == two && two > three) {
+            System.out.println("Первое и вторе числа имеют максимальное значение " + one );
+        }
+        else if (two == three && three > one) {
+            System.out.println("Второе и третье числа имеют максимальное значение " + two );
+        }
+        else if (one == three && three > two){
+            System.out.println("Первое и третье числа имеют максимальное значение " + one );
+        }
+        else if (one == two && two == three) {
+            System.out.println("Все числа равны!");
+        }
+
 
     }
 }
